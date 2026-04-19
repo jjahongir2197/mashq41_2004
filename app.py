4c137d556b6b6df2ed496f4701fb42f0d08283a9
Jahongir Jumaboyev <jjahongir2197@gmail.com>
2026-04-19 13:24:46 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    name = ""
+
+    if request.method == "POST":
+        name = request.form.get("name")
+
+    return render_template("index.html", name=name)
+
+app.run(debug=True)
